da38825fc41f9ad5081b9d08bb3f193869b0edda
ctare <sankaku900622@yahoo.co.jp>
2017-08-27 20:41:15 +0900

    add abstract  water mob

---
+++ src/main/java/com/example/examplemod/entities/mobs/common/EntityWaterMob.java
@@ -0,0 +1,13 @@
+package com.example.examplemod.entities.mobs.common;
+
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+/**
+ * Created by ctare on 2017/08/27.
+ */
+public abstract class EntityWaterMob extends EntityMob{
+    public EntityWaterMob(World p_i1738_1_) {
+        super(p_i1738_1_);
+    }
+}
